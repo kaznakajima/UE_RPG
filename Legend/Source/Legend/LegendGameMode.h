2154b858b591bcc83d447635ea30b6e199b3db12
kaznkjm <nkc.kazuya.nakajima@gmail.com>
2024-10-06 04:11:56 +0900

    プロジェクト初期化

---
+++ Legend/Source/Legend/LegendGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LegendGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ALegendGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ALegendGameMode();
+};
+
+
+
